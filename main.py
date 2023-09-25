17a1d8ad2160165d82cf1e2f1eafcc3011a7c38f
TRF_Batch_23 <rishihjain001@gmail.com>
2023-09-25 12:52:44 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from src.ml_project import logging
+from ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logging.info("Welcome to our custom logging")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+        logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
