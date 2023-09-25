1809ff25d64588b8145c0ae3394e57083ea4ad4d
TRF_Batch_23 <rishihjain001@gmail.com>
2023-09-25 20:26:24 +0530

    model evaluation and ml flow added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPip
 from ml_project.pipeline.stage_02_data_validation import DataValidationTraningPipeline
 from ml_project.pipeline.stage_03_data_transformation import DataTransformationTraningPipeline
 from ml_project.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from ml_project.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
 
@@ -46,6 +47,17 @@ try:
         obj = ModelTrainerTrainingPipeline()
         obj.main()
         logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logging.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logging.exception(e)
         raise e
